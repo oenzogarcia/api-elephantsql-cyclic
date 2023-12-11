d043f815ff54d7e5324d06df021c4ca42aef596c
Enzo Garcia <enzo.garcss@gmail.com>
2023-12-11 15:44:57 -0300

    conectado com db elephantsql

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const knex = require('./conexao');
 
 const app = express();
 
@@ -10,7 +11,13 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-    return res.json('tudo ok');
+    try {
+        const animes = await knex('animes').select('*');
+        return res.json(animes);
+    } catch (error) {
+        console.log(error.message);
+    }
+
 });
 
 app.listen(process.env.PORT, () => {
