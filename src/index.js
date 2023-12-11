a75f3ae08c607617a1d3b1cdd1af6a898293afcc
Enzo Garcia <enzo.garcss@gmail.com>
2023-12-11 15:56:46 -0300

    criada rota /animes

--- src/index.js
+++ src/index.js
@@ -20,6 +20,16 @@ app.get('/', async (req, res) => {
 
 });
 
+app.get('/animes', async (req, res) => {
+    try {
+        const animes = await knex('animes').select('*');
+        return res.json(animes);
+    } catch (error) {
+        console.log(error.message);
+    }
+
+});
+
 app.listen(process.env.PORT, () => {
     console.log(`Servidor em pé na porta: ${process.env.PORT}`)
 });
